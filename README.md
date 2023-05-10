0c52ab5e609419a6dcff2ff25e7cadacf4a4a03b
Adrian Sergheev <sergheevdev@icloud.com>
2023-05-10 18:43:16 +0200

    initial

---
+++ README.md
@@ -0,0 +1,5 @@
+# Updating App Data
+
+## Completed Project
+
+Explore the completed project for [Updating App Data](https://developer.apple.com/tutorials/app-dev-training/updating-app-data).
---
+++ Scrumdinger/Views/ScrumsView.swift
@@ -0,0 +1,39 @@
+/*
+ See LICENSE folder for this sample’s licensing information.
+ */
+
+import SwiftUI
+
+struct ScrumsView: View {
+    @Binding var scrums: [DailyScrum]
+    @State private var isPresentingNewScrumView = false
+    
+    var body: some View {
+        NavigationStack {
+            List($scrums) { $scrum in
+                NavigationLink(destination: DetailView(scrum: $scrum)) {
+                    CardView(scrum: scrum)
+                }
+                .listRowBackground(scrum.theme.mainColor)
+            }
+            .navigationTitle("Daily Scrums")
+            .toolbar {
+                Button(action: {
+                    isPresentingNewScrumView = true
+                }) {
+                    Image(systemName: "plus")
+                }
+                .accessibilityLabel("New Scrum")
+            }
+        }
+        .sheet(isPresented: $isPresentingNewScrumView) {
+            NewScrumSheet(scrums: $scrums, isPresentingNewScrumView: $isPresentingNewScrumView)
+        }
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: .constant(DailyScrum.sampleData))
+    }
+}
